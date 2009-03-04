69286562227415e653523100f2dadcb87158acea
rcmorano <rcmorano@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-03-04 10:24:44 +0000

    v6 testsys libraries

---
+++ testsys/custombuildsteps.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+
+from buildbot.steps.shell import ShellCommand, WithProperties
+from buildbot.steps.transfer import FileUpload
+from buildbot.status.builder import SUCCESS, FAILURE, WARNINGS
+
+from guadalinex import upload_dir, halt_on_lintian_error, halt_on_unittest_error
+from guadalinex import path, derivative, livehelper, pdebuild
+
+class RemoveSVN(ShellCommand):
+    """ Removes the .svn directories recursively"""
+
+    name = "RemoveSVN"
+    command = "rm -rf $(find -name .svn)"
+    description = [name]
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+class PBuildPkg(ShellCommand):
+    """Perfoms the building of a package with pdebuild
+    It counts and logs lintian error and lintian warnings.
+    On lintian errors can return FAILURE if guadalinex.halt_on_lintian_error 
+    it's True.
+    """
+
+    name = "PBuildPkg"
+    command = pdebuild
+    description = [name]
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+    def createSummary(self, log):
+        warning_log = []
+        error_log = []
+	self.descriptionDone = [self.name]
+
+        for line in log.readlines():
+            if line.strip().startswith("W:"):
+                warning_log.append(line)
+            if line.strip().startswith("E:"):
+                error_log.append(line)
+
+        self.warnings = len(warning_log)
+        self.errors = len(error_log)
+
+        if self.warnings:
+            self.addCompleteLog('Warnings', "".join(warning_log))
+            self.descriptionDone.append("warn=%d" % self.warnings)
+
+        if self.errors:
+            self.addCompleteLog('Errors', "".join(error_log))
+            self.descriptionDone.append("err=%d" % self.errors)
+
+    def evaluateCommand(self, cmd):
+	"""
+	Evaluates the result of pdebuild command.
+
+	If lintian errors are founded it can return
+	FAILURE or WARNINGS depending on guadalinex.halt_on_lintian_errors
+	"""
+        if cmd.rc != 0:
+            return FAILURE
+        if self.errors:
+	    if halt_on_lintian_error:
+	        return FAILURE
+	    else:
+		return WARNINGS
+        if self.warnings:
+            return WARNINGS
+        return SUCCESS
+
+
+class GCSBuild(PBuildPkg):
+    """It performs the building of a gcs package."""
+    name = "GCSBuild"
+    command = ["gcs_build"]
+    description = [name]
+
+    def __init__(self, **kwargs):
+	PBuildPkg.__init__(self, **kwargs)
+
+class SetSVNRev(ShellCommand):
+    """
+    In gcs packages, it sets the svn-revision as package version/revision.
+    """
+    name = "SetSVNRev"
+    command = ["sed", "-i", WithProperties("s/^version\:.*/version\: v6r%s/g", "got_revision"), "gcs/info"]
+    description = [name]
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+
+class Unittests(ShellCommand):
+    """
+    Run the package's unittest suite.
+    It run WARNINGS if can't find the 'unittests' executable.
+    It parses warnings and errors and can return FAILURE on unittest
+    errors if guadalinex.halt_on_unittest_errors it's True.
+    """
+    name = "Unittests"
+    command = ["bash", "unittests"]
+    description = [name]
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+    def createSummary(self, log):
+        warning_log = []
+        error_log = []
+	self.descriptionDone = [self.name]
+
+        for line in log.readlines():
+            if line.strip().startswith("WARNING:"):
+                warning_log.append(line)
+            if line.strip().startswith("FAIL:"):
+                error_log.append(line)
+
+        self.warnings = len(warning_log)
+        self.errors = len(error_log)
+
+        if self.warnings:
+            self.addCompleteLog('Warnings', "".join(warning_log))
+            self.descriptionDone.append("warn=%d" % self.warnings)
+
+        if self.errors:
+            self.addCompleteLog('Errors', "".join(error_log))
+            self.descriptionDone.append("err=%d" % self.errors)
+
+    def evaluateCommand(self, cmd):
+	"""
+	Evaluate the status of unittests execution
+
+	If there is no unittest executable the code 127 
+	it's retured. It throws WARNINGS.
+	"""
+
+	if cmd.rc == 127:
+		return WARNINGS
+        elif cmd.rc != 0:
+	    if halt_on_unittest_error:
+                return FAILURE
+	    else:
+		return WARNINGS
+
+        if self.warnings or self.errors:
+            return WARNINGS
+        return SUCCESS
+
+
+class UploadPkg(ShellCommand):
+    """
+    Copies the binary and source package from the slave
+    """
+    #TODO: Replace this code to use FileUpload using custom
+    # build properties has you can see in #87 buildbot ticket.
+    name = "UploadPkg"
+    #TODO: Fix this string ala python style
+    command = ["sh", "-c", 
+		    "cp ../*.dsc ../*.tar.gz ../*.udeb ../*.deb ../*.build ../*.changes %s; \
+		    rm -f ../*.dsc ../*.tar.gz ../*.udeb ../*.deb ../*.build ../*.changes"
+		    % upload_dir]
+    description = [name]
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+class UpdateDerivative(ShellCommand):
+    name = "UpdateDerivative"
+    command = derivative
+    description = [name]
+    error_log = None
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+    def createSummary(self, log):
+	warning_log = []
+
+	self.descriptionDone = [self.name]
+
+        for line in log.readlines():
+            if line.strip().startswith("Error:"):
+                self.error_log = line
+            if line.strip().startswith("Warning:"):
+		    warning_log.append(line)
+
+	self.warnings = len(warning_log)
+
+        if self.warnings:
+            self.addCompleteLog('Warnings', "".join(warning_log))
+            self.descriptionDone.append("warn=%d" % self.warnings)
+
+        if self.error_log:
+            self.descriptionDone.append("%s" % self.error_log)
+
+    def evaluateCommand(self, cmd):
+	if cmd.rc != 0:
+		return FAILURE
+
+	if self.error_log:
+            return FAILURE
+
+	if self.warnings:
+	    return WARNINGS
+
+        return SUCCESS
+
+class LiveHelper(ShellCommand):
+    name = "livehelper"
+    command = livehelper
+    description = [name]
+
+    def __init__(self, **kwargs):
+	ShellCommand.__init__(self, **kwargs)
+
+    def createSummary(self, log):
+        for line in log.readlines():
+            if line.strip().startswith("Error:"):
+                self.error_log = line
+
+    def evaluateCommand(self, cmd):
+	self.descriptionDone = [self.name]
+
+	if cmd.rc == 255:
+		self.descriptionDone.append(self.error_log)
+		return FAILURE
+
+	if cmd.rc != 0:
+		return FAILURE
---
+++ testsys/guadalinex.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+# Configuración útil para master.cfg
+
+############
+# SVN 
+############
+
+# URL del subversion sobre el que hacer polling.
+svn = "http://forja.guadalinex.org/guadalinexv6"
+
+# Tiempo de polling, en segundos, sobre el subversion para detectar cambios.
+polling_time = 2*60
+
+# Nombre del directorio que contendrá las apps. 
+# Las apps son ramas de subversion con tags y trunk y que se construyen 
+# con debuild. Solo se chequeará la última versión el tag sobre la que se
+# hayan aplicado commits. 
+apps_dir = "apps"
+
+# Nombre del directorio que contendrá los metapkgs.
+# Los metapkgs son ramas de subversion a construir con gcs_build.
+metapkgs_dir = "metapkgs"
+
+# Nombre de la subrama trunk.
+trunk_dir = "trunk"
+
+# Nombre de la subrama tags.
+tags_dir = "tags"
+
+# Nombre de la subrama gcs.
+gcs_dir = "gcs"
+
+# Lista de nombre de las apps. El nombre debe coincidir con la rama de 
+#subversion que contiene la app. La inclusión en esta lista de una nueva app
+#hace que buildbot la gestione automáticamente.
+#apps=[]
+apps=[
+        "sun-java6",
+]
+#	"buildbotdummy",
+#        "casper-guada",
+#	"dumphive",
+#        "escritorio-movistar",
+#        "g3g",
+#        "gcs",
+#        "gfxboot-theme-guada",
+#	"getatr",
+#	"guadafox",
+#	"guadalinex-eadmin",
+#        "guadalinex-keyring",
+#        "guadalinex-supplement-generator",
+#        "guadalinex-supplement-installer",
+#        "hermes",
+#        "lemurae",
+#        "lig",
+#	"ubiquity",
+#        "usplash-theme-guadalinex",
+#        "vodafone-mobile-connect-card-driver-for-linux",
+#        "accesibility-profiles",
+#        "gedit-plugin-tloleo",
+#        "gnome-panel",
+#        "gnome-system-tools",
+#        "gnome-volume-manager",
+#        "gru",
+#        "grubaker",
+#        "guada-ubiquity",
+#        "informe-diagnostico",
+#        "mount-systray",
+#        "opensc",
+#        "sun-java6-6-06",
+#        "watermain",
+#]
+
+# Lista de nombre de los metapkgs. El nombre debe coincidir con la rama de 
+# subversion que contiene el metapkg. La inclusión en esta lista de un nuevo 
+# metapkg hace que buildbot lo gestione automáticamente.
+metapkgs = [
+#	"guadalinex-about",
+#        "guadalinex-artwork",
+        "guadalinex-desktop",
+#        "guadalinex-desktop-conf",
+#	"guadalinex-example-content",
+        "guadalinex-minimal",
+#        "guadalinex-minimal-conf",
+        "guadalinex-standard",
+#        "guadalinex-standard-conf",
+#	"guadalinex-user-manual",
+#        "meta-guadalinex-v5",
+#        "suplemento-dvd-gv5",
+#        "suplemento-gv5-desarrollo",
+#        "suplemento-gv5-educativo",
+#        "suplemento-gv5-juegos",
+#        "suplemento-gv5-varios",
+]
+
+
+# Building 
+############
+
+# Tiempo de cortesía, en segundos,  entre que se detecta un cambio en el último
+# tag de la app y se comienza el proceso de integración.
+app_timer = 5
+
+# Tiempo de cortesía, en segundos, entre que se detecta un cambio en el metapkg
+# y se comienza el proceso de integración.
+metapkg_timer = 5
+
+# Gensys es ejecutado una vez al día. Defínase aquí la hora concreta de lanzamienzo.
+gensys_time = "00:00"
+
+# Indica si debemos abortar el proceso de integración ante errores 
+# de lintian.
+halt_on_lintian_error = False
+
+# Indica si debemos abortar el proceso de integración ante errores
+# de unittests.
+halt_on_unittest_error = True
+
+
+################
+# Gensys
+################
+
+# Directorio donde se subirán automáticamente los paquetes construidos.
+upload_dir = "/var/gensys/deb-repositories/gv6"
+
+# TODO: Esto debería ir en una clase a FileUpload a parte
+rawimage = "/var/gensys/live-helper/guadalinexv6/binary.iso"
+ftpimage = "/var/gensys/guadalinex-desktop-i386.iso"
+
+# Path común para la ejecución de los scripts gensys
+path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+# Script para generar el repositorio 'derivative' y variables de entorno
+# que necesite para su ejecución
+derivative = "update-derivative-repository"
+derivative_env = {
+    "PATH" : path
+}
+
+# Script live-helper y variables de entorno que necesite para su ejecución
+livehelper = "/var/gensys/live-helper/guadalinexv6/build.sh"
+livehelper_env = { 
+    "PATH" : path
+}
+
+# pdebuild custom command
+pdebuild = "pdebuild -- --basepath /var/gensys/cowbuilder/base-test.cow"

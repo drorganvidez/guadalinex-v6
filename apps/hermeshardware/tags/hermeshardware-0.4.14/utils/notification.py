bbacdf543c8c49fbb00e54c2e4f3d4469cfc7099
juanje <juanje@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-05-05 16:12:05 +0000

    Añadidos comentarios a utils/notification.py Añadido i18n a cadenas de log en utils/notification.py Añadida identificacion "HERMES: " al sumario de todos las notificaciones

--- apps/hermeshardware/tags/hermeshardware-0.4.14/utils/notification.py
+++ apps/hermeshardware/tags/hermeshardware-0.4.14/utils/notification.py
@@ -53,25 +53,24 @@ import logging
 import os
 import time
 
+from gettext import gettext as _
+
 if getattr(dbus, "version", (0, 0, 0)) >= (0, 41, 0):
     import dbus.glib
 
-
 class NotificationDaemon(object):
     """ 
-    [es] 
+    [es] Esta clase es una implementacion del demonio de notificaciones 
+         (notification-daemon)
     -----------------------------------------------------------------------
-    [en] 
-    """
-    """
-    This class is a wrapper for notification-daemon program.
+    [en] This class is a wrapper for notification-daemon program.
     """
 
     def __init__(self):
         """ 
-        [es] 
+        [es] Método de inicializacion
         -------------------------------------------------------------------
-        [en] 
+        [en] Intialization method
         """
         self.logger = logging.getLogger()
         bus = dbus.SessionBus()
@@ -80,21 +79,24 @@ class NotificationDaemon(object):
 
         self.iface = dbus.Interface(obj, 'org.freedesktop.Notifications')
 
-    # Main Message #######################################################
+    # [es] Mensaje principal
+    # [en] Main Message 
 
     def show(self, summary, message, icon, actions = {}): 
         """ 
-        [es] 
+        [es] Metodo para mostrar una informacion emergente en el area de 
+             usuario
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to show up information on the user Desktop
         """
         if actions != {}:
             timeout = 12000
             (notify_actions,action_handlers) = self.__process_actions(actions)
-            
+
             def action_invoked(nid, action_id):
                 if action_handlers.has_key(action_id) and res == nid:
-                    #Execute the action handler
+                    # [es] Se ejecuta el manejador de acciones
+                    # [en] Execute the action handler
                     thread.start_new_thread(action_handlers[action_id], ())
 
                 self.iface.CloseNotification(dbus.UInt32(nid))
@@ -102,88 +104,103 @@ class NotificationDaemon(object):
             condition = False
             while not condition:
                 try:
-                    self.logger.debug("Trying to connect to ActionInvoked")
+                    self.logger.debug(_("Trying to connect to ActionInvoked"))
                     self.iface.connect_to_signal("ActionInvoked", action_invoked)
                     condition = True
                 except:
-                    logmsg = "ActionInvoked handler not configured. "
-                    logmsg += "Trying to run notification-daemon."
+                    logmsg = _("ActionInvoked handler not configured.")
+                    logmsg += _("Trying to run notification-daemon.")
                     self.logger.warning(logmsg)
-                    os.system('/usr/lib/notification-daemon/notification-daemon &')                
+                    os.system('/usr/lib/notification-daemon/notification-daemon &')
                     time.sleep(0.2)
 
         else:
             timeout = 7000
-            #Fixing no actions messages
             notify_actions = []
 
+        # [es] Incluimos el nombre de HERMES en el titulo de la notificacion 
+        #      emergente
+        # [en] Include HERMES name on the notification title
+        summary = "HERMES: " + summary
         res = self.iface.Notify("Hermes", 
-        dbus.UInt32(0),
-                dbus.String(icon),
-                summary, 
-                message, 
-                notify_actions,
-        {},
-                dbus.UInt32(timeout))
+                                dbus.UInt32(0),
+                                dbus.String(icon),
+                                summary, 
+                                message, 
+                                notify_actions,
+                                {},
+                                dbus.UInt32(timeout))
         return res
 
-
-    # Specific messages #################################
+    # [es] Mensajes específicos
+    # [en] Specific messages 
 
     def show_info(self, summary, message, actions = {}):
         """ 
-        [es] 
+        [es] Metodo para mostrar notificacion informativa, incluimos icono 
+             de informacion de gtk por defecto
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to show up an informative notification, we include the 
+             default information gtk icon
         """
         return self.show(summary, message, "gtk-dialog-info", actions)
 
 
     def show_warning(self, summary, message, actions = {}):
         """ 
-        [es] 
+        [es] Metodo para mostrar notificacion de advertencia, incluimos 
+             icono de alerta de gtk por defecto
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to show up a warning notification, we include the 
+             default warning gtk icon
         """
         return self.show(summary, message, "gtk-dialog-warning", actions)
 
 
     def show_error(self, summary, message, actions = {}):
         """ 
-        [es] 
+        [es] Metodo para mostrar notificacion de error, incluimos icono 
+             de error de gtk por defecto
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to show up an error notification, we include the 
+             default error gtk icon
         """
         return self.show(summary, message, "gtk-dialog-error", actions)
 
 
     def close(self, nid):
         """ 
-        [es] 
+        [es] Método para cerrar la ventana  de notificación 
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to close the notification window
         """
         try:
             self.iface.CloseNotification(dbus.UInt32(nid))
         except:
             pass
-
-# Private methods ###################################
+    
+    # [es] Métodos privados
+    # [en] Private methods 
     def __process_actions(self, actions):
         """ 
-        [es] 
-        -------------------------------------------------------------------
-        [en] 
-        """
-        """
-        Devuelve una 2-tupla
+        [es] Devuelve una tupla:
 
-        La primera es una lista cuyos valores pares (comenzando por 0)
-    se refieren a la identificación de la acción y cuyos valores
-    impares serán la cadena a mostrar en el botón de la acción
+             El primer elemento es una lista cuyos valores pares (comenzando 
+             por 0) se refieren a la identificación de la acción y cuyos 
+             valores impares serán la cadena a mostrar en el botón de la 
+             acción
 
-        El segundo contiene como claves los identificadores (enteros) de las
-        acciones a tomar y como valores las funciones a ejecutar
+             El segundo contiene como claves los identificadores (enteros) 
+             de las acciones a tomar y como valores las funciones a ejecutar
+        --------------------------------------------------------------------
+        [en] Returns a tuple:
+
+             First element is a list. Odd position values (starting at 0)
+             refer to the action identifiers and even position values are
+             strings to show on the action button.
+
+             Second contains, as keys, actions to take identificators 
+             (integers), and as its values functions to execute.
         """
         if actions == {}:
             #FIXME
@@ -202,65 +219,66 @@ class NotificationDaemon(object):
 
 class FileNotification(object):
     """ 
-    [es] 
+    [es] Clase para escribir notificaciones a fichero
     -----------------------------------------------------------------------
-    [en] 
+    [en] Class to save notifications to file
     """
 
     def __init__(self, filepath):
         """ 
-        [es] 
+        [es] Método de inicializacion. Identificamos el fichero en el que 
+             vamos a escribir.
         -------------------------------------------------------------------
-        [en] 
+        [en] Initialization method. We identify the file to write in.
         """
         self.filepath = filepath
 
 
     def show (self, summary, body, icon, actions = {}):
         """ 
-        [es] 
+        [es] Método para escribir a fichero una notificacion
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to save to a file a notification
         """
-        self.__write("show: %s, %s, %s" % (summary, body, icon))
+        self.__write(_("show:") + " %s, %s, %s" % (summary, body, icon))
 
 
     def show_info(self, summary, body, actions = {}):
         """ 
-        [es] 
+        [es] Método para escribir a fichero una notificacion
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to save to a file a notification
         """
-        self.__write("show_info: %s, %s, %s" % (summary, body))
+        self.__write(_("show_info:") + " %s, %s, %s" % (summary, body))
 
 
     def show_warning(self, summary, body, actions = {}):
         """ 
-        [es] 
+        [es] Método para escribir a fichero una notificacion de advertencia
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to save to a file a warning notification
         """
-        self.__write("show_warning: %s, %s, %s" % (summary, body))
+        self.__write(_("show_warning:") + " %s, %s, %s" % (summary, body))
 
 
     def show_error(self, summary, body, actions = {}):
         """ 
-        [es] 
+        [es] Método para escribir a fichero una notificacion de error
         -------------------------------------------------------------------
-        [en] 
+        [en] Method to save to a file an error notification
         """
-        self.__write("show_error: %s, %s, %s" % (summary, body))
+        self.__write(_("show_error:") + " %s, %s, %s" % (summary, body))
 
 
     def __write(self, text):
         """ 
-        [es] 
+        [es] Método privado para escribir a fichero un texto
         -------------------------------------------------------------------
-        [en] 
+        [en] Private method to write to a file a given text
         """
         try:
             objfile = open(self.filepath, 'a')
             objfile.write(text + '\n')
             objfile.close()
         except Exception, e:
-            logging.getLogger().error('FileNotification: ' + e)
+            logging.getLogger().error(_("FileNotification: ") + e)

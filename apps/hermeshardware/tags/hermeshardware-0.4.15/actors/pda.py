190641cff838352b9e0f0473e2bcfb5ec15084ce
juanje <juanje@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-05-22 11:42:49 +0000

    Comentarios

--- apps/hermeshardware/tags/hermeshardware-0.4.15/actors/pda.py
+++ apps/hermeshardware/tags/hermeshardware-0.4.15/actors/pda.py
@@ -1,49 +1,52 @@
 # -*- coding: utf-8 -*-
-
-
-#Módulo bluetooth - Módulo que implementa el "actor hardware" para los
-#dispositivos PDA Windows Mobile
 #
-#Copyright (C) 2006 Junta de Andalucía
+# Authors:
+#     Gumersindo Coronel Pérez (gcoronel)
+#     Ghe Rivero <ghernando@yaco.es>
+#     Jose Chaso (pchaso) <jose.chaso at gmail>
 #
-#Autor/es (Author/s):
+# [es] Modulo usbtablet - Módulo que implementa el "actor hardware" para las
+#                         PDA con Windows Mobile
+# [en] usbtablet module - Implements hardware actor for Windows Mobile PDAs
 #
-#- Ghe Rivero <ghernando@yaco.es>
+# Copyright (C) 2009 Junta de Andalucía
 #
-#Este fichero es parte de Detección de Hardware de Guadalinex 2007
+# ----------------------------[es]---------------------------------------------
 #
-#Detección de Hardware de Guadalinex 2007  es software libre. Puede redistribuirlo y/o modificarlo 
-#bajo los términos de la Licencia Pública General de GNU según es 
-#publicada por la Free Software Foundation, bien de la versión 2 de dicha
-#Licencia o bien (según su elección) de cualquier versión posterior. 
+# Este fichero es parte de Detección de Hardware de Guadalinex V6
 #
-#Detección de Hardware de Guadalinex 2007  se distribuye con la esperanza de que sea útil, 
-#pero SIN NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL 
-#implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO 
-#PARTICULAR. Véase la Licencia Pública General de GNU para más detalles. 
+# Este programa es software libre: puede redistribuirlo y/o modificarlo bajo
+# los términos de la Licencia Pública General version 3 de GNU según
+# es publicada por la Free Software Foundation.
 #
-#Debería haber recibido una copia de la Licencia Pública General 
-#junto con Detección de Hardware de Guadalinex 2007 . Si no ha sido así, escriba a la Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+# Este programa se distribuye con la esperanza de que será útil, pero
+# SIN NINGUNA GARANTÍA, incluso sin la garantías implicitas de
+# MERCANTILIZACION, CALIDAD SATISFACTORIA o de CONVENIENCIA PARA UN PROPÓSITO
+# PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
 #
-#-------------------------------------------------------------------------
+# Debería haber recibido una copia de la Licencia Pública General
+# junto con este programa; si no ha sido así,
+# visite <http://www.gnu.org/licenses/>
+# o escriba a la Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 #
-#This file is part of Detección de Hardware de Guadalinex 2007 .
+# ----------------------------[en]---------------------------------------------
 #
-#Detección de Hardware de Guadalinex 2007  is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#at your option) any later version.
+# This file is part of Guadalinex V6 Hardware Detection.
 #
-#Detección de Hardware de Guadalinex 2007  is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
 #
-#You should have received a copy of the GNU General Public License
-#along with Foobar; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, visit <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import os.path
 
 from utils.synaptic import Synaptic
@@ -52,12 +55,16 @@ from gettext import gettext as _
 
 
 class Actor(PkgDeviceActor):
+    """ 
+    [es] Implementacion de clase Actor para tabletas digitalizadores USB
+    --------------------------------------------------------------------------
+    [en] Actor class implementation for USB Graphic tablets
+    """
     msg = """
     python: (value == 'Pocket PC PDA') or ('PocketLOOX' in value)
     """
     __required__ = {'info.product': msg,
                     'linux.subsystem': 'usb'}
-
     __icon_path__  = os.path.abspath('actors/img/wme.png')
     __iconoff_path__ = os.path.abspath('actors/img/wmeoff.png')
     __device_title__ = _('PDA')

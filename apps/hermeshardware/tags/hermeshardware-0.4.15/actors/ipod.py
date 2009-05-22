e49c831da037c343a1c9b7330b46d27f8a23071a
juanje <juanje@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-05-22 11:41:08 +0000

    Añadidos comentarios, eliminadas dependencias de PkgDeviceActor. Minima modificacion en cadena i18n Se deja en manos del escritorio (hecho en ubuntu) los mecanismos de lanzamiento de programas asociados al iPod Solo se notifica conexion y desconexion por lo que se deriva de DeviceActor.

--- apps/hermeshardware/tags/hermeshardware-0.4.15/actors/ipod.py
+++ apps/hermeshardware/tags/hermeshardware-0.4.15/actors/ipod.py
@@ -1,62 +1,69 @@
 # -*- coding: utf-8 -*-
-
-
-#Módulo ipod - Módulo que implementa el "actor hardware" para los
-#dispositivos ipod 
 #
-#Copyright (C) 2005 Junta de Andalucía
+# Authors:
+#     Gumersindo Coronel Pérez (gcoronel)
+#     J. Félix Ontañón <fontanon@emergya.es>
+#     Jose Chaso (pchaso) <jose.chaso at gmail>
 #
-#Autor/es (Author/s):
+# [es] Modulo IPOD - Módulo que implementa el "actor hardware" para los
+#                    reproductores multimedia iPod
+# [en] IPOD module - Implements hardware actor for iPod multimedia player
 #
-#- Gumersindo Coronel Pérez <gcoronel@emergya.info>
-#- J. Félix Ontañón <fontanon@emergya.es>
+# Copyright (C) 2009 Junta de Andalucía
 #
-#Este fichero es parte de Detección de Hardware de Guadalinex 2005 
+# ----------------------------[es]---------------------------------------------
 #
-#Detección de Hardware de Guadalinex 2005  es software libre. Puede redistribuirlo y/o modificarlo 
-#bajo los términos de la Licencia Pública General de GNU según es 
-#publicada por la Free Software Foundation, bien de la versión 2 de dicha
-#Licencia o bien (según su elección) de cualquier versión posterior. 
+# Este fichero es parte de Detección de Hardware de Guadalinex V6
 #
-#Detección de Hardware de Guadalinex 2005  se distribuye con la esperanza de que sea útil, 
-#pero SIN NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL 
-#implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO 
-#PARTICULAR. Véase la Licencia Pública General de GNU para más detalles. 
+# Este programa es software libre: puede redistribuirlo y/o modificarlo bajo
+# los términos de la Licencia Pública General version 3 de GNU según
+# es publicada por la Free Software Foundation.
 #
-#Debería haber recibido una copia de la Licencia Pública General 
-#junto con Detección de Hardware de Guadalinex 2005 . Si no ha sido así, escriba a la Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+# Este programa se distribuye con la esperanza de que será útil, pero
+# SIN NINGUNA GARANTÍA, incluso sin la garantías implicitas de
+# MERCANTILIZACION, CALIDAD SATISFACTORIA o de CONVENIENCIA PARA UN PROPÓSITO
+# PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
 #
-#-------------------------------------------------------------------------
+# Debería haber recibido una copia de la Licencia Pública General
+# junto con este programa; si no ha sido así,
+# visite <http://www.gnu.org/licenses/>
+# o escriba a la Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 #
-#This file is part of Detección de Hardware de Guadalinex 2005 .
+# ----------------------------[en]---------------------------------------------
 #
-#Detección de Hardware de Guadalinex 2005  is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#at your option) any later version.
+# This file is part of Guadalinex V6 Hardware Detection.
 #
-#Detección de Hardware de Guadalinex 2005  is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
 #
-#You should have received a copy of the GNU General Public License
-#along with Foobar; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, visit <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import os.path
 
-from utils.pkginstaller import PkgInstaller 
-from deviceactor import PkgDeviceActor
+#from utils.pkginstaller import PkgInstaller 
+from deviceactor import DeviceActor #PkgDeviceActor
 from gettext import gettext as _
 
-class Actor(PkgDeviceActor):
+class Actor(DeviceActor):#PkgDeviceActor):
+    """ 
+    [es] Implementacion de clase Actor para reproductor de musica iPod
+    --------------------------------------------------------------------------
+    [en] Actor class implementation for iPod music player
+    """
     __required__ = {'portable_audio_player.access_method.protocols': ['storage', 'ipod'],
                     'info.product': 'iPod'}
-
     __icon_path__  =  os.path.abspath('actors/img/ipod.png')
     __iconoff_path__ = os.path.abspath('actors/img/ipodoff.png')
     __device_title__ = _('iPod')
     __device_conn_description__ = _('iPod device detected')
-    __device_disconn_description__ = _('iPod device disconnected.')
+    __device_disconn_description__ = _('iPod device disconnected')

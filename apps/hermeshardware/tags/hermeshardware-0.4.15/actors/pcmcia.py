329c58dec3fafe387827aa5a93536e32e6e8a112
juanje <juanje@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-05-22 11:42:35 +0000

    Añadidos comentarios. Normalizacion a actor pasivo, heredando de DeviceActor, usando las propiedades de notificacion y eliminando los metodos on_added y on_removed por estar ya definidos en la clase base.

--- apps/hermeshardware/tags/hermeshardware-0.4.15/actors/pcmcia.py
+++ apps/hermeshardware/tags/hermeshardware-0.4.15/actors/pcmcia.py
@@ -1,68 +1,68 @@
 # -*- coding: utf-8 -*-
-
-#Módulo pcmcia- Módulo que implementa el "actor hardware" para los
-#dispositivos pcmcia
 #
-#Copyright (C) 2005 Junta de Andalucía
+# Authors:
+#     Gumersindo Coronel Pérez (gcoronel)
+#     Jose Chaso (pchaso) <jose.chaso at gmail>
 #
-#Autor/es (Author/s):
+# [es] Modulo pcmcia - Módulo que implementa el "actor hardware" para los
+#                         dispositivos PCMCIA
+# [en] pcmcia module - Implements hardware actor for PCMCIA devices
 #
-#- Gumersindo Coronel Pérez <gcoronel@emergya.info>
+# Copyright (C) 2009 Junta de Andalucía
 #
-#Este fichero es parte de Detección de Hardware de Guadalinex 2005 
+# ----------------------------[es]---------------------------------------------
 #
-#Detección de Hardware de Guadalinex 2005  es software libre. Puede redistribuirlo y/o modificarlo 
-#bajo los términos de la Licencia Pública General de GNU según es 
-#publicada por la Free Software Foundation, bien de la versión 2 de dicha
-#Licencia o bien (según su elección) de cualquier versión posterior. 
+# Este fichero es parte de Detección de Hardware de Guadalinex V6
 #
-#Detección de Hardware de Guadalinex 2005  se distribuye con la esperanza de que sea útil, 
-#pero SIN NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL 
-#implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO 
-#PARTICULAR. Véase la Licencia Pública General de GNU para más detalles. 
+# Este programa es software libre: puede redistribuirlo y/o modificarlo bajo
+# los términos de la Licencia Pública General version 3 de GNU según
+# es publicada por la Free Software Foundation.
 #
-#Debería haber recibido una copia de la Licencia Pública General 
-#junto con Detección de Hardware de Guadalinex 2005 . Si no ha sido así, escriba a la Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+# Este programa se distribuye con la esperanza de que será útil, pero
+# SIN NINGUNA GARANTÍA, incluso sin la garantías implicitas de
+# MERCANTILIZACION, CALIDAD SATISFACTORIA o de CONVENIENCIA PARA UN PROPÓSITO
+# PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
 #
-#-------------------------------------------------------------------------
+# Debería haber recibido una copia de la Licencia Pública General
+# junto con este programa; si no ha sido así,
+# visite <http://www.gnu.org/licenses/>
+# o escriba a la Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 #
-#This file is part of Detección de Hardware de Guadalinex 2005 .
+# ----------------------------[en]---------------------------------------------
 #
-#Detección de Hardware de Guadalinex 2005  is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#at your option) any later version.
+# This file is part of Guadalinex V6 Hardware Detection.
 #
-#Detección de Hardware de Guadalinex 2005  is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
 #
-#You should have received a copy of the GNU General Public License
-#along with Foobar; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, visit <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import os.path
 
 from deviceactor import DeviceActor
 from gettext import gettext as _
 
-PCMCIAICON = os.path.abspath('actors/img/pcmcia.png')
-PCMCIAICONOFF = os.path.abspath('actors/img/pcmciaoff.png')
 
 class Actor(DeviceActor):
-
-    __required__ = {
-    'info.subsystem' : 'pcmcia'
-    }
-
-    def on_added(self):
-        self.msg_render.show(_("PCMCIA"), _("PCMCIA device connected"),
-                PCMCIAICON)
-
-    def on_removed(self):
-        self.msg_render.show(_("PCMCIA"), _("PCMCIA device disconnected"),
-                PCMCIAICONOFF)
-
+    """ 
+    [es] Implementacion de clase Actor para dispositivos PCMCIA
+    --------------------------------------------------------------------------
+    [en] Actor class implementation for PCMCIA devices
+    """
+    __required__ = {'info.subsystem' : 'pcmcia'}
+    __icon_path__  = os.path.abspath('actors/img/pcmcia.png')
+    __iconoff_path__ = os.path.abspath('actors/img/pcmciaoff.png')
+    __device_title__ = _("PCMCIA")
+    __device_conn_description__ = _("PCMCIA device connected")
+    __device_disconn_description__ = _("PCMCIA device disconnected")
 

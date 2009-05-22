6187f779866ac43749e9e2f15209371ae3802aea
juanje <juanje@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-05-22 11:41:51 +0000

    Añadidos comentarios

--- apps/hermeshardware/tags/hermeshardware-0.4.15/actors/omnikeycardreader.py
+++ apps/hermeshardware/tags/hermeshardware-0.4.15/actors/omnikeycardreader.py
@@ -1,47 +1,54 @@
 # -*- coding: utf-8 -*-
+#
+# Authors:
+#     Gumersindo Coronel Pérez (gcoronel)
+#     J. Félix Ontañón <fontanon@emergya.es>
+#     Jose Chaso (pchaso) <jose.chaso at gmail>
+#
+# [es] Modulo omnikeycardreader - Módulo que implementa el "actor hardware" 
+#                        para los lectores de tarjetas Omnikey Cardman 3121
+# [en] omnikeycardreader module - Implements hardware actor for 
+#                        Omnikey Cardman 3121 cardreader
+#
+# Copyright (C) 2009 Junta de Andalucía
+#
+# ----------------------------[es]---------------------------------------------
+#
+# Este fichero es parte de Detección de Hardware de Guadalinex V6
+#
+# Este programa es software libre: puede redistribuirlo y/o modificarlo bajo
+# los términos de la Licencia Pública General version 3 de GNU según
+# es publicada por la Free Software Foundation.
+#
+# Este programa se distribuye con la esperanza de que será útil, pero
+# SIN NINGUNA GARANTÍA, incluso sin la garantías implicitas de
+# MERCANTILIZACION, CALIDAD SATISFACTORIA o de CONVENIENCIA PARA UN PROPÓSITO
+# PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+#
+# Debería haber recibido una copia de la Licencia Pública General
+# junto con este programa; si no ha sido así,
+# visite <http://www.gnu.org/licenses/>
+# o escriba a la Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+#
+# ----------------------------[en]---------------------------------------------
+#
+# This file is part of Guadalinex V6 Hardware Detection.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, visit <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#Módulo omnikeycardreader- Módulo que implementa el "actor hardware" para los
-#lectores de tarjetas Omnikey Cardman 3121
-#
-#Copyright (C) 2007 Junta de Andalucía
-#
-#Autor/es (Author/s):
-#
-#- J. Félix Ontañón <fontanon@emergya.es>
-#
-#Este fichero es parte de Detección de Hardware de Guadalinex 2005 
-#
-#Detección de Hardware de Guadalinex 2005  es software libre. Puede redistribuirlo y/o modificarlo 
-#bajo los términos de la Licencia Pública General de GNU según es 
-#publicada por la Free Software Foundation, bien de la versión 2 de dicha
-#Licencia o bien (según su elección) de cualquier versión posterior. 
-#
-#Detección de Hardware de Guadalinex 2005  se distribuye con la esperanza de que sea útil, 
-#pero SIN NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL 
-#implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO 
-#PARTICULAR. Véase la Licencia Pública General de GNU para más detalles. 
-#
-#Debería haber recibido una copia de la Licencia Pública General 
-#junto con Detección de Hardware de Guadalinex 2005 . Si no ha sido así, escriba a la Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
-#
-#-------------------------------------------------------------------------
-#
-#This file is part of Detección de Hardware de Guadalinex 2005 .
-#
-#Detección de Hardware de Guadalinex 2005  is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#at your option) any later version.
-#
-#Detección de Hardware de Guadalinex 2005  is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with Foobar; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import commands
 import os
@@ -55,7 +62,12 @@ from gettext import gettext as _
 CERTMANAGER_CMD = '/usr/bin/certmanager.py'
 
 class Actor(PkgDeviceActor):
-
+    """ 
+    [es] Implementacion de clase Actor para lectores de tarjetas Omnikey 
+         Cardman 3121
+    --------------------------------------------------------------------------
+    [en] Actor class implementation for Omnikey Cardman 3121 cardreader
+    """
     __required__ = {
             "info.subsystem": "usb_device",
             "usb_device.vendor_id":0x076b,

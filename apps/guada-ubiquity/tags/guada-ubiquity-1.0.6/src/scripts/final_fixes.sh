e73a5061fbd55a7899e62757c141b5ca8f12b461
rcmorano <rcmorano@f556af9c-ebad-4709-b871-15ab8a80d05a>
2009-05-27 16:25:41 +0000

    added openssh-server key regeneration

--- apps/guada-ubiquity/tags/guada-ubiquity-1.0.6/src/scripts/final_fixes.sh
+++ apps/guada-ubiquity/tags/guada-ubiquity-1.0.6/src/scripts/final_fixes.sh
@@ -8,6 +8,14 @@ function error() {
 	echo "Error: $*"
 }
 
+# set custom guadalinex sources.list
 cp -v /usr/share/guada-ubiquity/templates/sources.list /target/etc/apt/sources.list || error "copying sources.list"
+
+# force openssh-server to regenerate keys
+for file in ssh_host_dsa_key ssh_host_dsa_key.pub ssh_host_rsa_key ssh_host_rsa_key.pub
+do
+	test -f /target/etc/ssh/$file && rm -f /target/etc/ssh/$file
+done
+chroot /target dpkg-reconfigure openssh-server
 
 exit 0
